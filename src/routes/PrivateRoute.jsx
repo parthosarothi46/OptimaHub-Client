d32371c586d52e7769d9da0486dae460b45fea25
parthosarothi46 <parthosarothi46@gmail.com>
2025-01-28 21:38:54 +0600

    Repair Private Route

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,17 +2,12 @@ import { useAuth } from "@/context/AuthProvider";
 import { Navigate } from "react-router";
 
 const PrivateRoute = ({ children }) => {
-  const { user, loading } = useAuth();
+  const { user } = useAuth();
 
-  // Show a loading spinner if still fetching user data
-  if (loading) return <div>Loading...</div>;
-
-  // If the user is not authenticated, redirect to login
   if (!user) {
     return <Navigate to="/login" />;
   }
 
-  // If user is authenticated, render the children (which in this case will be the dashboard)
   return children;
 };
 
